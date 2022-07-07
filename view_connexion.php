f98b98f5000a93668c5900135e756cdcf52f90b4
DevMe97 <errazisalim@hotmail.com>
2022-07-07 12:13:47 +0200

    Ajout de texte

---
+++ view_connexion.php
@@ -0,0 +1,16 @@
+<html lang=fr>
+ <head>
+ <meta charset=UFT-8>
+ <title>connexion :</title>
+ </head>
+ <body>
+ <h1>Connexion : </h1>
+ <form action= method=poste>
+ <p>Veuillez saisir vote identifiant :</p>
+ <input type=texte name=login>
+ <p>Veuillez saisir votre mot de passe :</p>
+ <imput type=texte name=mdp>
+ <imput type=submit value=connexion>
+ </form>
+ </body>
+ </html>
